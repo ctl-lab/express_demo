c24baf7363cdcb02bf1f0bfae0be0445e8cffc91
emiliano-ma <emilianomainero@gmail.com>
2020-08-23 08:44:20 +0200

    final version of the app, after watching the demo.

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  console.table(req.query)
+  let greeting = req.query.e || 'World'
+  res.send(`<h1>Hello ${greeting}!</h1>`)
+})
+
+app.get('/emi', (req, res) => {
+    console.table(req.query)
+    res.send('<h1>Hello Emi!</h1>')
+  })
+
+app.listen(port, () => console.warn(`The audience is listening on port ${port}!`))
